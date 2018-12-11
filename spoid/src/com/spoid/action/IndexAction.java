37c4a1a01d7ea910ad9dc830410cbf793ea48848
ahdaerago <place0w0r@gmail.com>
2018-12-12 00:55:12 +0900

    movie index

--- spoid/src/com/spoid/action/IndexAction.java
+++ spoid/src/com/spoid/action/IndexAction.java
@@ -1,18 +1,31 @@
 package com.spoid.action;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.spoid.dao.MovieDAO;
+import com.spoid.dto.BoxOfficeDTO;
+import com.spoid.dto.DetailDTO;
 
 public class IndexAction implements Action
 {
 		public ActionForward excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 		{
 		  String url = "index.jsp";
+		  
+		  BoxOfficeDTO bDto = new BoxOfficeDTO();
+		  DetailDTO dDto = new DetailDTO();
 		 
-		 
+		  MovieDAO mDao = MovieDAO.getInstance();
+		  List<DetailDTO> list = mDao.indexMovie();
+		  
+		  System.out.println("====>index 페이지  조회 결과 : "+list.size());
+		  
+		  
 		  ActionForward forward = new ActionForward();
 		  forward.setPath(url);
 		  forward.setRedirect(false);
---
+++ spoid/src/com/spoid/dao/MovieDAO.java
@@ -0,0 +1,40 @@
+package com.spoid.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.spoid.dto.DetailDTO;
+import com.spoid.mybatis.SqlMapConfig;
+
+public class MovieDAO {
+	private MovieDAO() {
+	}
+	private static MovieDAO instance = new MovieDAO();
+	public static MovieDAO getInstance() {
+		return instance;
+	}
+	SqlSessionFactory sqlSessionFactory = SqlMapConfig.getSqlSession(); 
+	
+	//mapper 접급하기 위한 SqlSession
+	SqlSession sqlSession;
+	public List<DetailDTO> indexMovie(){
+		List<DetailDTO> list = new ArrayList<DetailDTO>();
+		
+		try {
+			System.out.println("인덱스 조회!!");
+			sqlSession = sqlSessionFactory.openSession();
+			list = sqlSession.selectList("indexList");
+					
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		
+		return list;
+	}
+}
