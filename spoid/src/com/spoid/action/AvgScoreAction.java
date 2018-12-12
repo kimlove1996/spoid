3021c2cdbd33e5cff5e64e910dab0a205244286a
sna8817 <jung71467470@gmail.com>
2018-12-12 16:23:59 +0900

    _몽고디비에서 네이버리뷰 평균값 출력하는 action단 _필요한 라이브러리jar: mongo

---
+++ spoid/src/com/spoid/action/AvgScoreAction.java
@@ -0,0 +1,56 @@
+package com.spoid.action;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+public class AvgScoreAction implements Action {
+
+	@Override
+	public ActionForward excute(HttpServletRequest paramHttpServletRequest,
+			HttpServletResponse paramHttpServletResponse) throws ServletException, IOException {
+		
+		
+		MongoClient mongoClient = new MongoClient("localhost",27017);
+		System.out.println("MongoClient Connected");
+		
+		MongoDatabase db = mongoClient.getDatabase("movie");
+		System.out.println("Get 'movie' MongoDatabase");
+		
+		MongoCollection<Document> collections = db.getCollection("naverreview");
+		System.out.println("데이터베이스명: "+db.getName());
+		
+		FindIterable<Document> iterate = collections.find();
+		MongoCursor<Document> cursor = iterate.iterator();
+		System.out.println("나와라아");
+		
+		// 평점평균 출력
+		while(cursor.hasNext()) {
+			Document document = cursor.next();
+
+			AggregateIterable<Document> iterable = collections.aggregate(Arrays.asList(
+		            new Document("$group", new Document("_id", new Document("movieCd", "$movieCd").append("total", "$sum") ).append("avgScore", new Document("$avg", "$score")))));
+				
+			for(Document doc : iterable) {
+				System.out.println(doc);
+			}
+			break;
+				
+		}
+		
+		return null;
+	}
+	
+}
