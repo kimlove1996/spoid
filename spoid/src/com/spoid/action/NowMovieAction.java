a6676a3c41e9435fcd3d96e6f3fc487f26309794
sna8817 <jung71467470@gmail.com>
2018-12-16 16:43:56 +0900

    _Action단 score출력 조회

--- spoid/src/com/spoid/action/NowMovieAction.java
+++ spoid/src/com/spoid/action/NowMovieAction.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.spoid.dao.MovieDAO;
+import com.spoid.dao.ReviewDAO;
 import com.spoid.dto.CriteriaMVDTO;
 import com.spoid.dto.DetailDTO;
 import com.spoid.dto.MoviePageDTO;
@@ -29,7 +30,78 @@ public class NowMovieAction  implements Action{
 		
 		System.out.println("=====>현재 상영 중인 영화 조회 결과 : "+list.size()+"개");
 		
-
+		
+		ReviewDAO rDao = ReviewDAO.getInstance();
+		  double nAvg[] = new double[list.size()];
+		  double dAvg[] = new double[list.size()];
+		  
+		  int nSvg[] = new int[list.size()];
+		  int dSvg[] = new int[list.size()];
+		  
+		  for (int i = 0; i < list.size(); i++) {
+			  
+			   nAvg[i] = rDao.scoreAvg("naver",list.get(i).getMovieCd());
+			   dAvg[i] = rDao.scoreAvg("daum",list.get(i).getMovieCd());
+			   
+			   if(nAvg[i]<=0) {
+				   nSvg[i]=0;
+			   }else if(nAvg[i]>0 && nAvg[i]<2) {
+				   System.out.println(nAvg[i]);
+				   nSvg[i] = 1;
+				   System.out.println("스포이드 >>>"+nSvg[i]);
+			   } else if(nAvg[i]>=2 && nAvg[i]<4) {
+				   System.out.println(nAvg[i]);
+				   nSvg[i] = 2;
+				   System.out.println("스포이드 >>>"+nSvg[i]);
+			   } else if(nAvg[i]>=4 && nAvg[i]<6) {
+				   System.out.println(nAvg[i]);
+				   nSvg[i] = 3;
+				   System.out.println("스포이드 >>>"+nSvg[i]);
+			   } else if(nAvg[i]>=6 && nAvg[i]<8) {
+				   System.out.println(nAvg[i]);
+				   nSvg[i] = 4;
+				   System.out.println("스포이드 >>>"+nSvg[i]);
+			   } else if(nAvg[i]>=8 && nAvg[i]<10) {
+				   System.out.println(nAvg[i]);
+				   nSvg[i] = 5;
+				   System.out.println("스포이드 >>>"+nSvg[i]);
+			   }
+			   
+			   if(dAvg[i]<=0) {
+				   dSvg[i]=0;
+			   } else if(dAvg[i]>0 && dAvg[i]<2) {
+				   System.out.println(dAvg[i]);
+				   dSvg[i] = 1;
+				   System.out.println("스포이드 >>>"+dSvg[i]);
+			   } else if(dAvg[i]>=2 && dAvg[i]<4) {
+				   System.out.println(dAvg[i]);
+				   dSvg[i] = 2;
+				   System.out.println("스포이드 >>>"+dSvg[i]);
+			   } else if(dAvg[i]>=4 && dAvg[i]<6) {
+				   System.out.println(dAvg[i]);
+				   dSvg[i] = 3;
+				   System.out.println("스포이드 >>>"+dSvg[i]);
+			   } else if(dAvg[i]>=6 && dAvg[i]<8) {
+				   System.out.println(dAvg[i]);
+				   dSvg[i] = 4;
+				   System.out.println("스포이드 >>>"+dSvg[i]);
+			   } else if(dAvg[i]>=8 && dAvg[i]<10) {
+				   System.out.println(dAvg[i]);
+				   dSvg[i] = 5;
+				   System.out.println("스포이드 >>>"+dSvg[i]);
+			   }
+		  }
+		  
+		  System.out.println("====>현상영작 페이지  조회 결과 : "+list.size());
+		  System.out.println(nAvg[1]+", "+dAvg[1]);
+		  System.out.println(nSvg[9]+"============"+dSvg[9]);
+		
+		
+		request.setAttribute("nAvg", nAvg);
+		request.setAttribute("dAvg", dAvg);
+		request.setAttribute("nSvg", nSvg);
+		request.setAttribute("dSvg", dSvg);
+		
 		request.setAttribute("nowMovie", list);
 
 
